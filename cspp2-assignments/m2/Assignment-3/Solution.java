8c164513bfd1862c2666a59622471fde84580147
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-28 23:33:08 +0530

    "check_style_score -> 4 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-    
+public final class Solution {
+
     /**
      * Constructs the object.
      */
@@ -18,30 +18,30 @@ public class Solution {
     }
 
     /**
-     * power function
+     * power function.
      *
      * @param      base      The base
      * @param      exponent  The exponent
      *
      * @return     base power exponent
      */
-    public static int power(int base, int exponent) {
-        if(exponent == 0) {
+    public static int power(final int base, final int exponent) {
+        if (exponent == 0) {
             return 1;
         }
         return base * power(base, exponent - 1);
     }
 
     /**
-     * main function
+     * main function.
      *
      * @param      args  is command line arguments
      */
     public static void main(final String[] args) {
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
-        int result=power(base,exponent);
+        int result = power(base, exponent);
         System.out.println(result);
     }
 }
