c5dfc1fd4afe2c1ea28b2c19105cbd1f630f9936
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-30 15:01:41 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -25,48 +25,45 @@ public class Solution {
      * @param      args  command line arguments
      */
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
-        
-        int r = input.nextInt();
-        int c = input.nextInt();
+
+        int r1 = input.nextInt();
+        int c1 = input.nextInt();
         input.nextLine();
-        
-        int[][] m1 = new int[r][c];
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+
+        int[][] m1 = new int[r1][c1];
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
                 m1[i][j] = input.nextInt();
             }
             input.nextLine();
         }
-        
-        int[][] m2 = new int[r][c];
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+
+        int r2 = input.nextInt();
+        int c2 = input.nextInt();
+        input.nextLine();
+
+        int[][] m2 = new int[r2][c2];
+        for (int i = 0; i < r2; i++) {
+            for (int j = 0; j < c2; j++) {
                 m2[i][j] = input.nextInt();
             }
             input.nextLine();
         }
-        
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                System.out.print(m1[i][j] + " ");
-            } System.out.println("");
+
+        if (r1 == r2 && c1 == c2) {
+            int[][] m = new int[r1][c1];
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c1; j++) {
+                    m[i][j] = m1[i][j] + m2[i][j];
+                    System.out.print(m[i][j] + " ");
+                } System.out.println("");
+            }
+        } else {
+            System.out.println("not possible");
         }
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                System.out.print(m2[i][j] + " ");
-            } System.out.println("");
-        }
 
-        int[][] m = new int[r][c];
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                m[i][j] = m1[i][j] + m2[i][j];
-                System.out.print(m[i][j] + " ");
-            } System.out.println("");
-        }
-    
     }
 }
