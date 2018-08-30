2e34c6c89ad3eb5e9789226d1bf39a95eb7ccbbe
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-30 14:30:39 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -0,0 +1,48 @@
+/**
+ * Date: 30-09-2018
+ * Author: Prudhvik
+ * Module 4 - Assignment 1
+ */
+
+import java.util.*;
+
+/**
+ * Class for solution.
+ */
+public class Solution {
+	/*
+	Fill this main function to print maximum of given array
+	*/
+
+	/**
+	 * function to find maximum in a given array.
+	 *
+	 * @param      arr   The array
+	 *
+	 * @return     maximum value in the array
+	 */
+	static int max(int[] arr) {
+		int maximum = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (maximum < arr[i]) {
+				maximum = arr[i];
+			}
+		}
+		return maximum;
+	}
+
+	/**
+	 * main function.
+	 *
+	 * @param      args  command line arguments
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt(); sc.nextLine();
+		}
+		System.out.println(max(arr));
+	}
+}
