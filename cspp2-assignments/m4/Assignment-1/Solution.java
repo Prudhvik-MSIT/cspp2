432f15db6ecdd85d10d0ac58da752320095dfb37
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-30 14:33:40 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -9,47 +9,47 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/*
-	Fill this main function to print maximum of given array
-	*/
+public final class Solution {
+    /*
+    Fill this main function to print maximum of given array
+    */
 
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		// empty constructor
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // empty constructor
+    }
 
-	/**
-	 * function to find maximum in a given array.
-	 *
-	 * @param      arr   The array
-	 *
-	 * @return     maximum value in the array
-	 */
-	static int max(int[] arr) {
-		int maximum = arr[0];
-		for (int i = 0; i < arr.length; i++) {
-			if (maximum < arr[i]) {
-				maximum = arr[i];
-			}
-		}
-		return maximum;
-	}
+    /**
+     * function to find maximum in a given array.
+     *
+     * @param      arr   The array
+     *
+     * @return     maximum value in the array
+     */
+    static int max(final int[] arr) {
+        int maximum = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (maximum < arr[i]) {
+                maximum = arr[i];
+            }
+        }
+        return maximum;
+    }
 
-	/**
-	 * main function.
-	 *
-	 * @param      args  command line arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); sc.nextLine();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt(); sc.nextLine();
-		}
-		System.out.println(max(arr));
-	}
+    /**
+     * main function.
+     *
+     * @param      args  command line arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); sc.nextLine();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt(); sc.nextLine();
+        }
+        System.out.println(max(arr));
+    }
 }
