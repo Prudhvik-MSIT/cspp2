74bb361e648b6190f8e8196767fec491ea580c34
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-30 14:31:10 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -38,7 +38,7 @@ public class Solution {
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); sc.nextInt();
+		int n = sc.nextInt(); sc.nextLine();
 		int[] arr = new int[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt(); sc.nextLine();
